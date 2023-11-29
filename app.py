fa047a747b5dd565bc9f010f16b8f03a316528a7
Yasmin Hackbart <90115526+yasminhackbart@users.noreply.github.com>
2023-11-29 16:47:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,19 +10,28 @@ def index():
 def predict_emotion():
     
     # Obtenha a entrada de texto do requisição POST 
-   
+    input_text = request.json.get("text")  
     
     if not input_text:
         # Resposta a enviar se o input_text for indefinido
-       
+        response = {
+                    "status": "error",
+                    "message": "Digite um texto para prever a emoção associada a ele!"
+                  }
+        return jsonify(response)
+    else:  
+        predicted_emotion,predicted_emotion_img_url = predict(input_text)
         
         # Resposta a enviar se o input_text não for indefinido
-        
+        response = {
+                    "status": "success",
+                    "data": {
+                            "predicted_emotion": predicted_emotion,
+                            "predicted_emotion_img_url": predicted_emotion_img_url
+                            }  
+                   }
+
         # Enviar resposta         
-        
+        return jsonify(response)
        
 app.run(debug=True)
-
-
-
-    
